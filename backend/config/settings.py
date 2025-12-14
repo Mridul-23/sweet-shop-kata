7ab91ffe5dfa1c3ebf6062517289f5db767f40a3
Mridul <140313918+Mridul-23@users.noreply.github.com>
2025-12-14 18:36:31 +0530

    chore(security): move Django secret key to environment variables
    Rotated the Django SECRET_KEY and removed hardcoded secrets from settings.
    Added an example environment file to document required variables.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# loads variables from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4yc)cy4$-@#4_x-i+c%e)$7z(mbc&0se+x1t#&!4)cv@4ul^k7"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
