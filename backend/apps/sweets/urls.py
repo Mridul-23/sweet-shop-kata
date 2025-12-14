2a7749c2abc99725e423aeee6199ecf5e02375e1
Mridul <140313918+Mridul-23@users.noreply.github.com>
2025-12-14 16:54:20 +0530

    feat(sweets): implement sweet purchase endpoint

--- backend/apps/sweets/urls.py
+++ backend/apps/sweets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SweetCreateView
+from .views import SweetCreateView, SweetPurchaseView
 
 urlpatterns = [
   path("sweets/", SweetCreateView.as_view(), name="sweet-create"),
+  path("sweets/<int:pk>/purchase/", SweetPurchaseView.as_view(), name="sweet-purchase"),
 ]
--- backend/apps/sweets/views.py
+++ backend/apps/sweets/views.py
@@ -1,4 +1,9 @@
-from rest_framework import generics, permissions
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from .models import Sweet
 from .serializers import SweetSerializer
 
@@ -6,4 +11,26 @@ from .serializers import SweetSerializer
 class SweetCreateView(generics.CreateAPIView):
   queryset = Sweet.objects.all()
   serializer_class = SweetSerializer
-  permission_classes = [permissions.IsAdminUser]
+  permission_classes = [IsAdminUser]
+
+
+
+class SweetPurchaseView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        sweet = get_object_or_404(Sweet, pk=pk)
+
+        if sweet.quantity <= 0:
+            return Response(
+                {"detail": "Sweet out of stock"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        sweet.quantity -= 1
+        sweet.save()
+
+        return Response(
+            {"message": "Purchase successful", "quantity": sweet.quantity},
+            status=status.HTTP_200_OK
+        )
